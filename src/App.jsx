019746b16099f2ee649398653591375760a8b2f8
Nittu Rajkumar <nitturajkumar123@gmail.com>
2026-03-20 19:16:10 +0530

    Update API endpoint to production URL

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     setError('');
     setResult(null);
     try {
-      const response = await fetch('http://localhost:5000/api/summarize', {
+      const response = await fetch('https://assignment-summarizer-server.onrender.com/api/summarize', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
